0530e59a18dd47b0170136f32abc4b551f841824
mayank0508 <thakurmayank0508@gmail.com>
2021-09-16 00:40:58 +0530

    yo mf this shits good man

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() { // this called the state constructor
+     super();
+
+
+this.state = {
+things: 'Hi 👋'
+}
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi 🥂</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p>{this.state.things}</p>
+          <button onClick={() => {this.setState({things: 'Bye MF 🤏'})}}>Check this shit out 🥵</button>
+          </header>
       </div>
     );
   }
